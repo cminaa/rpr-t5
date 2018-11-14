39d6ff544eaf11cf74f1d49797e333de4160dc34
Korisnik <eradusic1@etf.unsa.ba>
2018-11-14 13:23:04 +0100

    gotov izgled digitrona

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,24 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
-public class Controller {
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
 
+public class Controller {
+    private SimpleStringProperty rezultat;
+
+    public  Controller(){
+        rezultat=new SimpleStringProperty();
+        rezultat.set("0");
+    }
+    
+    public SimpleStringProperty rezultatProperty() {
+        return rezultat;
+    }
+    public String getRezultat(){
+        return rezultat.get();
+    }
+
+    public void ispisi(ActionEvent actionEvent) {
+        rezultat.set("");
+    }
 }
